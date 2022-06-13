fdff6be02c08657b1cb46fc81800bcdcdb2a436e
Ahmed <sanniahmed908@gmail.com>
2022-06-13 17:18:47 +0100

    first commit

---
+++ RPS_task.py
@@ -0,0 +1,32 @@
+import random
+
+rock = ("r")
+paper = ("p")
+scissors = ("s")
+
+
+user_action = input("Enter a choice (r, p, s): ")
+
+possible_actions = ["r", "p", "s"]
+computer_action = random.choice(possible_actions)
+
+print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+
+if user_action == computer_action:
+    print(f"Both players selected {user_action}. It's a tie! Try again....")
+elif user_action == "r":
+    if computer_action == "s":
+        print("Rock smashes scissors! You win!")
+    else:
+        print("Paper covers rock! You lose.")
+elif user_action == "p":
+    if computer_action == "r":
+
+        print("Paper covers rock! You win!")
+    else:
+        print("Scissors cuts paper! You lose.")
+elif user_action == "s":
+    if computer_action == "p":
+        print("Scissors cuts paper! You win!")
+    else:
+        print("Rock smashes scissors! You lose.")
